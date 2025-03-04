9f5ee5c8832adad2f37ea0d6204137e72dea363d
louaylh10 <hcinlouay@gmail.com>
2025-03-04 17:23:58 +0100

    static

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,4 +1,11 @@
+export async function generateStaticParams() {
+    return [
+      { id: 'signin' },  
+      { id: 'login' },
+      { id: 'forgotpwd' }
+    ]; 
+}
+
 export default function PostPage({ params }) {
     return <h1>Post ID: {params.id}</h1>;
-  }
-  
+}
