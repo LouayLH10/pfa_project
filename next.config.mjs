11b82d2597fbd4d392a8f17da253c7d4069fd418
louaylh10 <hcinlouay@gmail.com>
2025-03-04 17:18:18 +0100

    package

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Nécessaire pour éviter des erreurs avec GitHub Pages
+  },
+  basePath: "/mon-repo", // Remplace par le nom de ton repo GitHub
+  assetPrefix: "/mon-repo/",
   reactStrictMode: true,
 };
 
