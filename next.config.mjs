87ceb038a3976bed0c50dce0848d8d3ba7e001b9
louaylh10 <hcinlouay@gmail.com>
2025-03-04 17:37:19 +0100

    v1

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, // Nécessaire pour éviter des erreurs avec GitHub Pages
-  },
-  basePath: "/pfa_project", // Remplace par le nom de ton repo GitHub
-  assetPrefix: "/pfa_project/",
   reactStrictMode: true,
 };
 
--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,11 +1,4 @@
-export async function generateStaticParams() {
-    return [
-      { id: 'signin' },  
-      { id: 'login' },
-      { id: 'forgotpwd' }
-    ]; 
-}
-
 export default function PostPage({ params }) {
     return <h1>Post ID: {params.id}</h1>;
-}
+  }
+  
