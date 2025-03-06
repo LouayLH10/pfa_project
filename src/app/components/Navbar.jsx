568e3c3ddc63752931de51e22293d72d2c058961
louaylh10 <hcinlouay@gmail.com>
2025-03-06 13:26:14 +0100

    About

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,32 @@
+"use client";
 import Link from 'next/link';
 import Button from '../ui/Button';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
 export default function Navbar() {
+
+const path=usePathname();
+useEffect(()=>{
+  const nav=document.querySelectorAll(".nav li")
+  nav.forEach((item) => {
+    item.style.color = "#14213d";
+  });
+  if(path==="/"){
+   
+    document.querySelector(".home").style.color="#fca311"
+  }
+  else if(path==="/about"){
+     document.querySelector(".about").style.color="#fca311"
+  }
+})
+
   return (
     <nav className="bg-white border-gray-200 light:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="/logo.png" className="h-9" alt="Flowbite Logo"  />
       <span className="self-center text-2xl font-semibold whitespace-nowrap light:text-white">
-        LH Learning
+        LH Learning 
       </span>
     </Link>
 
@@ -21,7 +40,7 @@ export default function Navbar() {
             id="user-dropdown"
           >
             <div className="px-4 py-3">
-              <span className="block text-sm text-gray-900 light:text-white">
+              <span className="block text-sm text-[#14213d] light:text-white">
                 Bonnie Green
               </span>
               <span className="block text-sm text-gray-500 truncate light:text-gray-400">
@@ -32,7 +51,7 @@ export default function Navbar() {
               <li>
                 <Link
                   href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 light:hover:bg-gray-600 light:text-gray-200 light:hover:text-white"
+                  className="block px-4 py-2 text-sm text-[#14213d] hover:bg-gray-100 light:hover:bg-gray-600 light:text-[#14213d] light:hover:text-white"
                 >
                   Dashboard
                 </Link>
@@ -40,7 +59,7 @@ export default function Navbar() {
               <li>
                 <Link
                   href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 light:hover:bg-gray-600 light:text-gray-200 light:hover:text-white"
+                  className="block px-4 py-2 text-sm text-[#14213d] hover:bg-gray-100 light:hover:bg-gray-600 light:text-[#14213d] light:hover:text-white"
                 >
                   Settings
                 </Link>
@@ -48,7 +67,7 @@ export default function Navbar() {
               <li>
                 <Link
                   href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 light:hover:bg-gray-600 light:text-gray-200 light:hover:text-white"
+                  className="block px-4 py-2 text-sm text-[#14213d] hover:bg-gray-100 light:hover:bg-gray-600 light:text-[#14213d] light:hover:text-white"
                 >
                   Earnings
                 </Link>
@@ -56,7 +75,7 @@ export default function Navbar() {
               <li>
                 <Link
                   href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 light:hover:bg-gray-600 light:text-gray-200 light:hover:text-white"
+                  className="block px-4 py-2 text-sm text-[#14213d] hover:bg-gray-100 light:hover:bg-gray-600 light:text-[#14213d] light:hover:text-white"
                 >
                   Sign out
                 </Link>
@@ -92,11 +111,11 @@ export default function Navbar() {
           className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           id="navbar-user"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white light:bg-gray-800 md:light:bg-gray-900 light:border-gray-700">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white light:bg-gray-800 md:light:bg-gray-900 light:border-gray-700 menu">
             <li>
               <Link
                 href="/"
-                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:light:text-blue-500"
+                className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-[#14213d] md:p-0 md:light:text-blue-500 md:hover:text-[#fca311] home"
                 aria-current="page"
               >
                 Home
@@ -104,8 +123,8 @@ export default function Navbar() {
             </li>
             <li>
               <Link
-                href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700"
+                href="/about"
+                className="block py-2 px-3 text-[#14213d] rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700 about"
               >
                 About
               </Link>
@@ -113,23 +132,23 @@ export default function Navbar() {
             <li>
               <Link
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700"
+                className="block py-2 px-3 text-[#14213d] rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700 courses"
               >
-                Services
+                Courses
               </Link>
             </li>
             <li>
               <Link
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700"
+                className="block py-2 px-3 text-[#14213d] rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700 blog"
               >
-                Pricing
+                Our Blog
               </Link>
             </li>
             <li>
               <Link
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700"
+                className="block py-2 px-3 text-[#14213d] rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#fca311] md:p-0 light:text-white md:light:hover:text-blue-500 light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700 contact"
               >
                 Contact
               </Link>
---
+++ src/app/ui/Entetepge.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Entetepge({page,path}) {
+  return (
+    <div className='entetepage'>
+<h1>{page}</h1>
+<br/>
+<p>Home{path}</p>
+
+    </div>
+  )
+}
+
+export default Entetepge
